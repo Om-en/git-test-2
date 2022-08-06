60977fc3eca4c845cafb7a1b405a06efa37eea3f
DARIS <darisilham77@gmail.com>
2022-08-06 11:24:50 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 </head>
 <body>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate eum accusamus labore minima iusto, quos nihil facilis, excepturi dolor earum pariatur dolorum autem sapiente aut explicabo soluta voluptatibus repellendus maxime.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
